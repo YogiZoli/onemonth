2379a561dc05e171cce588c2cb682eef7c946962
YogiZoli <zoltanp.gero@gmail.com>
2014-09-22 22:34:54 +0000

    Pins are designed with Masonary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,15 @@ Workspace::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
   
   config.action_mailer.default_url_options = { :host => 'http://onemonth-yogizoli.herokuapp.com/' }
+  
+  # This sets Papaerclip to upload to AmazonS3 in production
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
